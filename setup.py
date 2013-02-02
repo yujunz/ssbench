eb7d6c432cef34bb5a74530486566d343c39f56a
Darrell Bishop <darrell@swiftstack.com>
2013-02-02 11:45:58 -0800

    Version bump to 0.0.9

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def readme():
 
 setup(
     name='ssbench',
-    version='0.0.8',
+    version='0.0.9',
     description='SwiftStack Swift Benchmarking Suite',
     long_description=readme(),
     license='Apache License (2.0)',
