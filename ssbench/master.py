4347aed42ea8c397a52ce9192ddb830e72134366
Darrell Bishop <darrell@swiftstack.com>
2013-11-07 09:00:36 -0800

    Remove unused variable.

--- ssbench/master.py
+++ ssbench/master.py
@@ -137,7 +137,7 @@ class Master(object):
 
     def do_a_run(self, concurrency, job_generator, result_processor,
                  auth_kwargs, mapper_fn=None, label='', noop=False,
-                 batch_size=1, run_results=None, delete_after=None):
+                 batch_size=1, run_results=None):
 
         if label and not self.quiet:
             print >>sys.stderr, label + """
