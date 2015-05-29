bcf9a3ff4cceb71040fe725df61c9f5ef5d7c1b3
Darrell Bishop <darrell@swiftstack.com>
2015-05-29 08:00:24 -0700

    Don't require newer pyzmq than SwiftStack currently uses
    Also bump verison.

--- ssbench/__init__.py
+++ ssbench/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-_version = (0, 3, 3)
+_version = (0, 3, 4)
 version = '.'.join(map(str, _version))
 
 # Constant names (which happen to need to match methods called
