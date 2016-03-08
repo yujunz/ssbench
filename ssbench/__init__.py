2f3451a4b28784f8445eea803ed97368640145a3
Timur Alperovich <timuralp@swiftstack.com>
2016-03-08 12:26:36 -0800

    Bump the version for the pypi release.

--- ssbench/__init__.py
+++ ssbench/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-_version = (0, 3, 7, 2)
+_version = (0, 3, 8)
 version = '.'.join(map(str, _version))
 
 # Constant names (which happen to need to match methods called
